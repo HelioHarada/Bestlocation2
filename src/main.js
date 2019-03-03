4492fe4c8116b38020ea905b656b0c8d2afc62e3
Hélio Harada <helioharada30@gmail.com>
2019-03-03 16:59:23 -0300

    use Router

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import Header from './Header.vue'
 import Index from './Index.vue'
 import Footer from './Footer.vue'
 
+// Vue router
+import VueRouter from 'vue-router';
+Vue.use(VueRouter)
+
 new Vue({
   el: '#header',
   render: h => h(Header)
