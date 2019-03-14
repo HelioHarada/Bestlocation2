3988fb0f38868526e7cec90b0f1fd7f982bbc735
Hélo Harada <helioharada30@gmail.com>
2019-03-13 22:03:03 -0300

    arruma ai leo

--- server/config/database.js
+++ server/config/database.js
@@ -4,10 +4,10 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const url = process.env.MONGOLAB_URI;
 console.log(url);
-mongoose.connect('mongodb://bestadmin:Admin1@ds053449.mlab.com:53449/heroku_kmb7qhcw', { useNewUrlParser: true })
+//mongoose.connect('mongodb://bestadmin:Admin1@ds053449.mlab.com:53449/heroku_kmb7qhcw', { useNewUrlParser: true })
 
 // conexão no banco
-//mongoose.connect('mongodb://localhost/bdBestLocation', { useNewUrlParser: true } );
+mongoose.connect('mongodb://localhost/bdBestLocation', { useNewUrlParser: true } );
 
 // ouvindo eventos que ocorrem em sucesso ou erro de conexão
 mongoose.connection.on('connected', () => {
--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
+
 // importa configuração do Express e do banco de dados
-const app = require('./server/config/express')();
-require('./server/config/database');
+const app = require('./config/express')();
+require('./config/database');
 
 require('dotenv').config()
+
+
 
 // passa a porta definida no Express e levantamento do servidor
 app.listen(app.get('port'), () => {
