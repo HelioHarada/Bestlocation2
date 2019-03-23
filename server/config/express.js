b381e13da22853149f84ecf641049878cd7e02a4
Leonardo Molina <leomolina0501@hotmail.com>
2019-03-22 21:48:34 -0300

    Configuração Header API

--- server/config/express.js
+++ server/config/express.js
@@ -6,6 +6,26 @@ var bodyParser = require('body-parser');
 // exportando função de configuração que retorna a variável 'app'
 module.exports = () => {
     var app = express();
+
+    app.use(function (req, res, next) {
+
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', '*');
+    
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+    
+        // Pass to next layer of middleware
+        next();
+    });
+
     app.set('port', (process.env.PORT || 8080));
 
     app.use(bodyParser.urlencoded({ extended: true }));    // parse application/x-www-form-urlencoded
