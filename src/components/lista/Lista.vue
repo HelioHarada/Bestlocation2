2b921e7d76a675d2fa85d3409890b5dc0122765d
Hélo Harada <helioharada30@gmail.com>
2019-03-23 20:01:21 -0300

    show lista ok

--- src/components/lista/Lista.vue
+++ src/components/lista/Lista.vue
@@ -1,24 +1,37 @@
 <template>
-    <h2>lista</h2>
+<div class="row content-lista" >
+  <div class="col-md-4 card-house" v-for="imovel in imoveis" v-bind:key="imovel">
+    <div class="card">
+       <img class="card-img-top" src="/../src/img/casa.jpg" alt="Card image cap">
+       <div class="card-body card-imovel">
+         <h4 class="card-title" >{{imovel.titulo}}</h4>
+         <h6 class="card-title" >{{imovel.status}} : R$ {{imovel.preco}}</h6>
+         <p class="card-text">Descrição: {{imovel.descricao}}</p>
+         <p class="card-text">Quartos: {{imovel.numQuartos}} / Banheiros: {{imovel.numBanheiros}}</p>
+         <p class="card-text">Endereço: {{imovel.endereco}} - {{imovel.cidade}}</p>
+         <!-- <p class="card-text">id: {{imovel._id}}</p> --> 
+         <a class="btn button-plus" @click="getId(imovel._id)"  data-toggle="modal" data-target="#desc-modal" >Mais+</a>
+        </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
-// data(){
-//         this.$http.get('/someUrl').then(response => {
 
-// // get body data
-// this.someData = response.body;
-
-// }, response => {
-// // error callback
-// });
+  data(){
+        return{
+            imoveis: [],
+        }
+  },
 
   created() {
 
-    let promise = this.$http.get('http://localhost:8080/api/users');
+    let promise = this.$http.get('http://localhost:8080/api/imoveis');
     promise .then(function(res) {
-        console.log(res);
+        console.log(res.body[3]);
+         this.imoveis = res.body
     });
   }
 
