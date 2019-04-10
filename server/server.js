46894a361dce07b3f81a9c780b2db3d54c9ad4f6
Unknown <helioharada30@gmail.com>
2019-04-10 14:44:44 -0300

    tentando subir

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 
 // importa configuração do Express e do banco de dados
 const app = require('./config/express')();
+var path = require('path')
+var serveStatic = require('serve-static')
 require('./config/database');
 
 require('dotenv').config()
 
 
+
+var app = express()
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
 // passa a porta definida no Express e levantamento do servidor
 app.listen(app.get('port'), () => {
