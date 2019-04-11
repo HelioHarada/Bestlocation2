70a2dadab51862db5925ff4c18f7bc9c547377f0
Unknown <helioharada30@gmail.com>
2019-04-11 14:40:17 -0300

    teste

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ require('dotenv').config()
 // app.get(/.*/, function(req, res){
 //     res.sendfile(__dirname + "dist/index.html")
 // })
-
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'bestolocation', 'index.html');
+    res.sendFile(index);
+  });
 
 
 // app.get('*', function (req, res) {
