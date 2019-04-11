4aa0ade0128281826f036f4578c6219d8eab0e6d
Unknown <helioharada30@gmail.com>
2019-04-11 15:46:57 -0300

    w

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 
 
 app.get('*', function(req, res){
-    res.sendFile('index.html', {root : path.join(__dirname + "../../bestlocation/dist")})
+    res.sendFile('index.html', {root : path.join(__dirname + "dist")})
 })
 app.use('/',serveStatic(path.join(__dirname + "/dist")));
 
