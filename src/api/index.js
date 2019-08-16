82aef95c76f0dad5482b27390f32636b3eb2c073
Harada <helioharada30@gmail.com>
2019-08-16 00:06:12 -0300

    subir correção

--- src/api/index.js
+++ src/api/index.js
@@ -26,7 +26,8 @@ export const store = {
 
 // get imoveis
 export function getImoveis () {
-  return this.$http.get(url+'/api/imovel/buscar?pageNo=1&size=5');
+    // return this.$http.get(url+'/api/imovel/buscar?pageNo=1&size=5');
+  return this.$http.get(url+'/api/imovel/buscar');
 }
 
 // delete imoveis 
